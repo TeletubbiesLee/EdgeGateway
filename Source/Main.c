c7fb93a1a02aaed52f28c91b501470e5912fa9ff
lilei <lilei@sojoline.com>
2020-04-24 14:17:03 +0800

    添加paho mqtt前

--- Source/Main.c
+++ Source/Main.c
@@ -79,6 +79,7 @@ int main(int argc, char *argv[])
 		SetProcessCloseSignal();		//父进程关闭之后，子进程也全部关闭
 
 		printf("AirQualitySensor (pid:%d) creat\n", getpid());
+		if(0)
 		AirQualitySensor(&airQualitySensor);							//六合一空气质量传感器
 		printf("AirQualitySensor (pid:%d) exit\n", getpid());
 
